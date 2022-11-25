8353b4529a2439699ad3c586371891284a4498de
Mikhail014 <msemin704@gmail.com>
2022-11-25 19:10:09 +0300

    Перемещение файлов в папку Lesson1

--- Homeworks/task1.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Введите число: "))
-
-if num > 5 and num < 8:
-    print(f"{num} - выходной")
-elif num < 1 or num > 7:
-    print(f"{num} - это не день недели")
-else:
-    print(f"{num} - это будний день")
