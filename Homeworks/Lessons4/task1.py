2e07003c6badbeb16a0cd0d9567a2c7d29093bb8
Mikhail014 <msemin704@gmail.com>
2022-12-04 00:35:44 +0300

    Задание 1. Число Пи. v.2

--- Homeworks/Lessons4/task1.py
+++ Homeworks/Lessons4/task1.py
@@ -3,6 +3,7 @@ pi = 3.141
 my_pi = 1
 count = 0
 i = 3
+pi_precision = None
 
 while True:
       if count % 2 == 0:
@@ -13,7 +14,7 @@ while True:
       diff = pi - round(my_pi * 4, 3)
 
       if d >= diff >= -d:
-            print(f"{pi - round(my_pi * 4, 3)}")
+            pi_precision = f"{pi - round(my_pi * 4, 3)}"
             break
 
       count += 1
@@ -21,4 +22,5 @@ while True:
 
 my_pi = round(my_pi * 4, 3)
 
-print(f"{my_pi}")
+print(f"Точность: {pi_precision}")
+print(f"Найденное число Пи: {my_pi}")
