0772adb905c05c56e4256a11f4695ec7456004e4
Mikhail014 <msemin704@gmail.com>
2022-12-13 18:04:24 +0300

    Задание 3. Change name text file

--- Homeworks/Lesson6/task1_3.py
+++ Homeworks/Lesson6/task1_3.py
@@ -1,11 +1,11 @@
 text = None
 
-with open("task1_text.txt", "r", encoding="utf-8") as file:
+with open("task1_text", "r", encoding="utf-8") as file:
     text = file.readline()
 
 textArr = text.split()
 
-with open("task1_text.txt", "w", encoding="utf-8") as file:
+with open("task1_text", "w", encoding="utf-8") as file:
     ### filter, lambda
     res = list(filter(lambda x: "абв" not in x, textArr))
     file.write(" ".join(res))
