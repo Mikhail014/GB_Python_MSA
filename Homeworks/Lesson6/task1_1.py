40a0b6afd90bbb209f50f34d6bb050ac2e2a9c07
Mikhail014 <msemin704@gmail.com>
2022-12-13 17:29:21 +0300

    Задание 1. List comp, enumerate

---
+++ Homeworks/Lesson6/task1_1.py
@@ -0,0 +1,25 @@
+"""""""""
+Задайте список из нескольких чисел. 
+Напишите программу, которая найдёт сумму элементов списка, 
+стоящих на нечётной позиции.
+Пример:
+- [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+"""""""""
+
+from random import randint
+
+### list comp
+arr_nums = [randint(1, 10) for i in range(0, 10)]
+
+print("Список:")
+print(arr_nums)
+
+sum_items = 0
+size = len(arr_nums)
+
+### enumerate
+for i, v in enumerate(arr_nums):
+    if i % 2 != 0:
+        sum_items += v
+
+print(f"Сумма нечетных позиций: {sum_items}")
